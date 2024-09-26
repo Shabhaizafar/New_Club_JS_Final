3e01c65bc5e392f4b7c7917bb6c018ad05fbaca1
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-26 18:46:44 +0530

    DOM7

---
+++ DOM/DOM7.js
@@ -0,0 +1,20 @@
+const submit = document.querySelector('#submit');
+
+
+//onclick,onmouseOver
+            //  event,function
+submit.addEventListener('click',
+    (e)=>{
+        e.preventDefault();
+        const t1 = document.querySelector('table');
+        
+        // How to add Child element using JS ?
+        const tr1 = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const text = document.querySelector('#first-name').value;
+        td1.append(text);
+        tr1.appendChild(td1);
+        t1.appendChild(tr1);
+        console.log(t1);
+    });
+
