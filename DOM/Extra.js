1e1feacec7677ef7c688303a21697e4a2ac54c56
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-06 17:44:20 +0530

    rps Completed

--- DOM/Extra.js
+++ DOM/Extra.js
@@ -1,16 +1,26 @@
 var u_main = document.querySelectorAll('main')[0];
 const c_main = document.querySelectorAll('main')[1];
-
-const Arr = ["url('1.png')","Paper","Scissor"];
+var span  = document.querySelector('span');
+var audio = new Audio('audio.mp3');
+const Arr = ["url('rock.png')","url('paper.png')","url('scissor.png')"];
 function btnclick(value){
     if(value=='rock'){
-        u_main.style.backgroundImage = "url('1.png')";
+        u_main.style.backgroundImage = "url('rock.png')";
         c_main.style.backgroundImage = `${Arr[parseInt(Math.random()*3)]}`;
     }else if(value=='scissor'){
-        u_main.innerHTML = 'Scissor';
-        c_main.innerHTML = `${Arr[parseInt(Math.random()*3)]}`;
+        u_main.style.backgroundImage = "url('scissor.png')";
+        c_main.style.backgroundImage = `${Arr[parseInt(Math.random()*3)]}`;
     }else if(value == "paper"){
-        u_main.innerHTML = 'Paper';
-        c_main.innerHTML = `${Arr[parseInt(Math.random()*3)]}`;
+        u_main.style.backgroundImage = "url('paper.png')";
+        c_main.style.backgroundImage = `${Arr[parseInt(Math.random()*3)]}`;
+    }
+
+
+    if(u_main.style.backgroundImage.valueOf() != c_main.style.backgroundImage.valueOf()){
+        //r and s
+        if(u_main.style.backgroundImage.valueOf().length==15 && c_main.style.backgroundImage.valueOf().length==18){
+            span.innerHTML= parseInt(span.innerHTML)+1;
+            audio.play();
+        }
     }
 }
