dd43b8f4b5433e0206b21c64c292dc40cf3089c4
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-19 14:38:53 +0530

    Promise1

---
+++ Asyncronouns/Promise.js
@@ -0,0 +1,25 @@
+// Successful
+// Pending 
+//UnSuccessful
+
+
+// var data = +prompt("Enter the Value of N :");
+
+// var mypromise = new Promise(function(Successful,Reject){
+//     if(data%5==0 && data%11==0){
+//         Successful();
+//     }
+//     else{
+//         Reject();
+//     }
+// });
+
+// mypromise.then(()=>{console.log('Successful')},()=>{console.log("Reject")});
+
+//////////////////////////////////////////////
+// Create a simple Promise that resolves after 2 seconds.
+
+// Example: How would you write a Promise that resolves with the message "Hello, World!" after a 2-second delay?
+// Create a Promise that rejects after 1 second.
+
+// Example: Write a Promise that rejects with the error "Something went wrong" after 1 second.
