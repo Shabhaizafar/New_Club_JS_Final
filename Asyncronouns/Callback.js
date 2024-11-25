a405a5cf2504026c8b32fea5bc67928ebb12e3c4
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-25 14:43:35 +0530

    promise

---
+++ Asyncronouns/Callback.js
@@ -0,0 +1,67 @@
+// var setNo = 1;
+function main(){
+    console.log("Function Execute1 !!!");
+}
+
+function main2(){
+    console.log("Function Execute2 !!!");
+}
+
+// setTimeout(() => {
+//     setNo = 1;
+//     main();
+//     console.log(setNo);
+//     setTimeout(() => {
+//         setNo = 2;
+//         main();
+//         console.log(setNo);
+//         setTimeout(() => {
+//             setNo = 3;
+//             main();
+//             console.log(setNo);
+//             setTimeout(() => {
+//                 setNo = 4;
+//                 main();
+//                 console.log(setNo);
+//                 setTimeout(() => {
+//                     setNo = 5;
+//                     main();
+//                     console.log(setNo);
+//                     setTimeout(() => {
+//                         setNo = 6;
+//                         main();
+//                         console.log(setNo);
+//                         setTimeout(() => {
+//                             setNo = 7;
+//                             main();
+//                             console.log(setNo);
+//                             setTimeout(() => {
+//                                 setNo = 8;
+//                                 main();
+//                                 console.log(setNo);
+//                                 setTimeout(() => {
+//                                     setNo = 9;
+//                                     console.log(setNo);
+//                                 }, 2000);
+//                             }, 2000);
+//                         }, 2000);
+//                     }, 2000);
+//                 }, 2000);
+//             }, 2000);
+//         }, 2000);    
+//     }, 2000);
+// }, 2000);
+
+// meggie , masala, water ,............ 
+
+// var userinfo = 0;
+// var mypromise  = new Promise((solve,reject)=>{
+//     if(userinfo){
+//         solve();
+//     }else{
+//         reject();
+//     }
+// });
+
+
+// mypromise.then(main,main2);
