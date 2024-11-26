c228c9ac91fff5bd2240953a37c5dcf3ebd80403
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-26 14:34:45 +0530

    promise

---
+++ Asyncronouns/Promise2.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('button');
+var count = 0;
+btn.addEventListener('click',function(){
+    count++;
+    const myPromise = new Promise((success,reject)=>{
+        if(count>=10){
+            success();
+        }else{
+            reject();
+        }
+    });
+    
+    
+    myPromise.then(()=>{
+        console.log("Promise Successfully Done!!!",count);
+    },()=>{
+        console.log("Promise Wait until count reach at 10!!",count);
+    });
+});
+
+
+
+
+
+// random  :    >    50 promise done 
+//              <       promise waiting
