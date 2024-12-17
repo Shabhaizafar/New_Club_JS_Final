b62b67ae8d5ddb8502de2fc37b7e0b1c6f820d9f
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-17 14:44:57 +0530

    API

---
+++ Asyncronouns/API/All1.js
@@ -0,0 +1,29 @@
+// const btn = document.querySelector('button');
+
+// btn.addEventListener("click",function(){
+//     navigator.geolocation.getCurrentPosition(myLoco);
+// });
+
+// function myLoco(){
+//     console.log(location.coords);
+// }
+
+// ---------------------
+// const myElement = document.getElementById("demo");
+
+
+// document.querySelector("button").addEventListener('click', function() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     myElement.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// });
+
+// function showPosition(position) {
+//     console.log("Running");
+//   myElement.innerHTML = "Latitude: " + position.coords.latitude +
+//   "<br>Longitude: " + position.coords.longitude;
+// }
+
+console.log(navigator);
